854b7e10851b2ff4a44e96e50af13ff52ca3e784
Alex <alxsar98@gmail.com>
2021-06-28 17:37:54 +0300

    feat: example page

---
+++ src/components/ExampleComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="example">
+    <h2>VUE Project</h2>
+    <h3>vuex, router, scss</h3>
+    <ul>
+      <li>eslint + prettier</li>
+      <li>sass-loader (global style variables)</li>
+      <li>axios</li>
+      <li>normalize.css</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ExampleComponent",
+};
+</script>
+
+
+<style scoped lang="scss">
+.example {
+  color: $my-color;
+  text-align: center;
+
+  & ul {
+    text-align: left;
+    color: $my-color2;
+  }
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <ExampleComponent />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import ExampleComponent from "@/components/ExampleComponent";
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
-  },
+  components: { ExampleComponent },
 };
 </script>
+
+<style scoped lang="scss">
+.home {
+  display: flex;
+  height: 100%;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
+
+
